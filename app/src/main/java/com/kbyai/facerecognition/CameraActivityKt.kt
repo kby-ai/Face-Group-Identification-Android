29224dac472e58e497af1424939555c4de0750b8
ml13571 <jackiewang13571@gmail.com>
2025-08-07 19:35:55 -0700

    implemented 2 face identification

--- app/src/main/java/com/kbyai/facerecognition/CameraActivityKt.kt
+++ app/src/main/java/com/kbyai/facerecognition/CameraActivityKt.kt
@@ -1,11 +1,15 @@
 package com.kbyai.facerecognition
 
 import android.Manifest
+import android.annotation.SuppressLint
 import android.content.Context
 import android.content.Intent
 import android.content.pm.PackageManager
 import android.os.Bundle
 import android.util.Size
+import android.view.View
+import android.widget.TextView
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import androidx.camera.core.CameraSelector
 import androidx.core.app.ActivityCompat
@@ -20,7 +24,6 @@ import io.fotoapparat.parameter.Resolution
 import io.fotoapparat.preview.Frame
 import io.fotoapparat.preview.FrameProcessor
 import io.fotoapparat.selector.front
-import io.fotoapparat.selector.back
 import io.fotoapparat.view.CameraView
 
 class CameraActivityKt : AppCompatActivity() {
@@ -44,21 +47,12 @@ class CameraActivityKt : AppCompatActivity() {
         cameraView = findViewById(R.id.preview)
         faceView = findViewById(R.id.faceView)
 
-        if (SettingsActivity.getCameraLens(context) == CameraSelector.LENS_FACING_BACK) {
-            fotoapparat = Fotoapparat.with(this)
-                .into(cameraView)
-                .lensPosition(back())
-                .frameProcessor(FaceFrameProcessor())
-                .previewResolution { Resolution(PREVIEW_HEIGHT,PREVIEW_WIDTH) }
-                .build()
-        } else  {
-            fotoapparat = Fotoapparat.with(this)
-                .into(cameraView)
-                .lensPosition(front())
-                .frameProcessor(FaceFrameProcessor())
-                .previewResolution { Resolution(PREVIEW_HEIGHT,PREVIEW_WIDTH) }
-                .build()
-        }
+        fotoapparat = Fotoapparat.with(this)
+            .into(cameraView)
+            .lensPosition(front())
+            .frameProcessor(FaceFrameProcessor())
+            .previewResolution { Resolution(PREVIEW_HEIGHT,PREVIEW_WIDTH) }
+            .build()
 
         if (ContextCompat.checkSelfPermission(this, Manifest.permission.CAMERA)
             == PackageManager.PERMISSION_DENIED
@@ -102,6 +96,7 @@ class CameraActivityKt : AppCompatActivity() {
 
     inner class FaceFrameProcessor : FrameProcessor {
 
+        @SuppressLint("SetTextI18n")
         override fun process(frame: Frame) {
 
             if(recognized == true) {
@@ -126,39 +121,93 @@ class CameraActivityKt : AppCompatActivity() {
             }
 
             if(faceBoxes.size > 0) {
-                val faceBox = faceBoxes[0]
-                if (faceBox.liveness > SettingsActivity.getLivenessThreshold(context)) {
-                    val templates = FaceSDK.templateExtraction(bitmap, faceBox)
+                if(faceBoxes.size == 2){
+                    val faceBox0 = faceBoxes[0]
+                    val faceBox1 = faceBoxes[1]
+                    if (faceBox0.liveness > SettingsActivity.getLivenessThreshold(context) && faceBox1.liveness > SettingsActivity.getLivenessThreshold(context)) {
+                        val templates0 = FaceSDK.templateExtraction(bitmap, faceBox0)
+                        val templates1 = FaceSDK.templateExtraction(bitmap, faceBox1)
 
-                    var maxSimiarlity = 0f
-                    var maximiarlityPerson: Person? = null
-                    for (person in DBManager.personList) {
-                        val similarity = FaceSDK.similarityCalculation(templates, person.templates)
-                        if (similarity > maxSimiarlity) {
-                            maxSimiarlity = similarity
-                            maximiarlityPerson = person
+                        var maxSimiarlity0 = 0f
+                        var maximiarlityPerson0: Person? = null
+                        var maxSimiarlity1 = 0f
+                        var maximiarlityPerson1: Person? = null
+
+                        for (person in DBManager.personList) {
+                            val similarity = FaceSDK.similarityCalculation(templates0, person.templates)
+                            if (similarity > maxSimiarlity0) {
+                                maxSimiarlity0 = similarity
+                                maximiarlityPerson0 = person
+                            }
+                        }
+
+                        for (person in DBManager.personList) {
+                            val similarity = FaceSDK.similarityCalculation(templates1, person.templates)
+                            if (similarity > maxSimiarlity1) {
+                                maxSimiarlity1 = similarity
+                                maximiarlityPerson1 = person
+                            }
+                        }
+
+                        if (maxSimiarlity0 > SettingsActivity.getIdentifyThreshold(context) && maxSimiarlity1 > SettingsActivity.getIdentifyThreshold(context)) {
+                            recognized = true
+                            val identifiedPerson0 = maximiarlityPerson0
+                            val identifiedSimilarity0 = maxSimiarlity0
+                            val identifiedPerson1 = maximiarlityPerson1
+                            val identifiedSimilarity1 = maxSimiarlity1
+
+                            runOnUiThread {
+                                val faceImage0 = Utils.cropFace(bitmap, faceBox0)
+                                val intent = Intent(context, ResultActivity::class.java)
+                                intent.putExtra("identified_face_0", faceImage0)
+                                intent.putExtra("enrolled_face_0", identifiedPerson0!!.face)
+                                intent.putExtra("identified_name_0", identifiedPerson0!!.name)
+                                intent.putExtra("similarity_0", identifiedSimilarity0)
+                                intent.putExtra("liveness_0", faceBox0.liveness)
+//                                intent.putExtra("yaw", faceBox.yaw)
+//                                intent.putExtra("roll", faceBox.roll)
+//                                intent.putExtra("pitch", faceBox.pitch)
+
+                                val faceImage1 = Utils.cropFace(bitmap, faceBox1)
+                                intent.putExtra("identified_face_1", faceImage1)
+                                intent.putExtra("enrolled_face_1", identifiedPerson1!!.face)
+                                intent.putExtra("identified_name_1", identifiedPerson1!!.name)
+                                intent.putExtra("similarity_1", identifiedSimilarity1)
+                                intent.putExtra("liveness_1", faceBox1.liveness)
+                                startActivity(intent)
+                            }
                         }
                     }
-                    if (maxSimiarlity > SettingsActivity.getIdentifyThreshold(context)) {
-                        recognized = true
-                        val identifiedPerson = maximiarlityPerson
-                        val identifiedSimilarity = maxSimiarlity
+                } else {
+                    val faceBox = faceBoxes[0]
+                    if (faceBox.liveness > SettingsActivity.getLivenessThreshold(context)) {
+                        val templates = FaceSDK.templateExtraction(bitmap, faceBox)
 
-                        runOnUiThread {
-                            val faceImage = Utils.cropFace(bitmap, faceBox)
-                            val intent = Intent(context, ResultActivity::class.java)
-                            intent.putExtra("identified_face", faceImage)
-                            intent.putExtra("enrolled_face", identifiedPerson!!.face)
-                            intent.putExtra("identified_name", identifiedPerson!!.name)
-                            intent.putExtra("similarity", identifiedSimilarity)
-                            intent.putExtra("liveness", faceBox.liveness)
-                            intent.putExtra("yaw", faceBox.yaw)
-                            intent.putExtra("roll", faceBox.roll)
-                            intent.putExtra("pitch", faceBox.pitch)
-                            startActivity(intent)
+                        var maxSimiarlity = 0f
+                        var maximiarlityPerson: Person? = null
+                        for (person in DBManager.personList) {
+                            val similarity = FaceSDK.similarityCalculation(templates, person.templates)
+                            if (similarity > maxSimiarlity) {
+                                maxSimiarlity = similarity
+                                maximiarlityPerson = person
+                            }
+                        }
+
+                        if (maxSimiarlity > SettingsActivity.getIdentifyThreshold(context)){
+                            runOnUiThread {
+                                val tv = findViewById<View>(R.id.notify) as TextView
+                                val identifiedPerson = maximiarlityPerson
+                                tv.text = "Now one face ${identifiedPerson?.name!!} was identified\nPlease try with 2 faces to get more details "
+                            }
+                        } else {
+                            runOnUiThread {
+                                val tv = findViewById<View>(R.id.notify) as TextView
+                                tv.text = "Now you are trying with one face\nPlease try with 2 faces to get more details "
+                            }
                         }
                     }
                 }
+
             }
         }
     }
--- app/src/main/java/com/kbyai/facerecognition/ResultActivity.kt
+++ app/src/main/java/com/kbyai/facerecognition/ResultActivity.kt
@@ -11,22 +11,34 @@ class ResultActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_result)
 
-        val identifyedFace = intent.getParcelableExtra("identified_face") as? Bitmap
-        val enrolledFace = intent.getParcelableExtra("enrolled_face") as? Bitmap
-        val identifiedName = intent.getStringExtra("identified_name")
-        val similarity = intent.getFloatExtra("similarity", 0f)
-        val livenessScore = intent.getFloatExtra("liveness", 0f)
-        val yaw = intent.getFloatExtra("yaw", 0f)
-        val roll = intent.getFloatExtra("roll", 0f)
-        val pitch = intent.getFloatExtra("pitch", 0f)
+        val identifyedFace0 = intent.getParcelableExtra("identified_face_0") as? Bitmap
+        val enrolledFace0 = intent.getParcelableExtra("enrolled_face_0") as? Bitmap
+        val identifiedName0 = intent.getStringExtra("identified_name_0")
+        val similarity0 = intent.getFloatExtra("similarity_0", 0f)
+        val livenessScore0 = intent.getFloatExtra("liveness_0", 0f)
+//        val yaw = intent.getFloatExtra("yaw", 0f)
+//        val roll = intent.getFloatExtra("roll", 0f)
+//        val pitch = intent.getFloatExtra("pitch", 0f)
 
-        findViewById<ImageView>(R.id.imageEnrolled).setImageBitmap(enrolledFace)
-        findViewById<ImageView>(R.id.imageIdentified).setImageBitmap(identifyedFace)
-        findViewById<TextView>(R.id.textPerson).text = "Identified: " + identifiedName
-        findViewById<TextView>(R.id.textSimilarity).text = "Similarity: " + similarity
-        findViewById<TextView>(R.id.textLiveness).text = "Liveness score: " + livenessScore
-        findViewById<TextView>(R.id.textYaw).text = "Yaw: " + yaw
-        findViewById<TextView>(R.id.textRoll).text = "Roll: " + roll
-        findViewById<TextView>(R.id.textPitch).text = "Pitch: " + pitch
+        val identifyedFace1 = intent.getParcelableExtra("identified_face_1") as? Bitmap
+        val enrolledFace1 = intent.getParcelableExtra("enrolled_face_1") as? Bitmap
+        val identifiedName1 = intent.getStringExtra("identified_name_1")
+        val similarity1 = intent.getFloatExtra("similarity_1", 0f)
+        val livenessScore1 = intent.getFloatExtra("liveness_1", 0f)
+
+        findViewById<ImageView>(R.id.imageEnrolled).setImageBitmap(enrolledFace0)
+        findViewById<ImageView>(R.id.imageIdentified).setImageBitmap(identifyedFace0)
+        findViewById<TextView>(R.id.textPerson).text = "Identified 1: " + identifiedName0
+        findViewById<TextView>(R.id.textSimilarity).text = "Similarity 1: " + similarity0
+        findViewById<TextView>(R.id.textLiveness).text = "Liveness score 1: " + livenessScore0
+//        findViewById<TextView>(R.id.textYaw).text = "Yaw: " + yaw
+//        findViewById<TextView>(R.id.textRoll).text = "Roll: " + roll
+//        findViewById<TextView>(R.id.textPitch).text = "Pitch: " + pitch
+
+        findViewById<ImageView>(R.id.imageEnrolled1).setImageBitmap(enrolledFace1)
+        findViewById<ImageView>(R.id.imageIdentified1).setImageBitmap(identifyedFace1)
+        findViewById<TextView>(R.id.textPerson1).text = "Identified 1: " + identifiedName1
+        findViewById<TextView>(R.id.textSimilarity1).text = "Similarity 1: " + similarity1
+        findViewById<TextView>(R.id.textLiveness1).text = "Liveness score 1: " + livenessScore1
     }
 }
